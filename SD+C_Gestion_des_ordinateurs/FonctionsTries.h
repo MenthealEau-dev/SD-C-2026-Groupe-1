ac1c404b18c25a599f74f3dcf8b62ac063d67cf8
Fortunatus <patriceuro10@gmail.com>
2026-06-16 22:05:18 +0000

    First commit

---
+++ SD+C_Gestion_des_ordinateurs/FonctionsTries.h
@@ -0,0 +1,14 @@
+#include "MesFonctions.h"
+#define TAB_MAX 300
+
+// prototype 5 de la fonction de repartition par spécialité
+
+void repartitonSpecialite (Tordinateur **listeOrdinateur, int nbreOrdi);
+
+// prototype 6 de la fonction de répartition par marque
+
+void repartitionMarque (Tordinateur **listeOrdinateur, int nbreOrdi);
+
+// prototype 7 de la fonction de tri par vitesse de processeurs
+
+void trier_parVitesse(Tordinateur **listeOrdinateur, int nbreOrdi);
